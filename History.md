fd54107a3fc708445b030cbb83cd81b3818a5998
Jake Luer <jake.luer@gmail.com>
2011-12-15 08:02:26 -0500

    Release 0.1.0

--- History.md
+++ History.md
@@ -1,4 +1,39 @@
 
+0.1.0 / 2011-12-15
+==================
+
+  * failing on purpose successful .. back to normal
+  * testing travis failure
+  * assert#arguments getter
+  * readme typo
+  * updated README
+  * added travis and npmignore
+  * copyright notices … think i got them all
+  * moved expect interface to own file for consistency
+  * assert ui deepEqual
+  * browser tests expect (all working)
+  * browser version built
+  * chai.fail (should ui)
+  * expect tests browser compatible
+  * tests for should and expect (all pass)
+  * moved fail to primary export
+  * should compatibility testing
+  * within, greaterThan, object, keys,
+  * Aliases
+  * Assertion#property now correctly works with negate and undefined values
+  * error message language matches should
+  * Assertion#respondTo
+  * Assertion now uses inspect util
+  * git ignore node modules
+  * should is exported
+  * AssertionError __proto__ from Error.prototype
+  * add should interface for should.js compatibility
+  * moved eql to until folder and added inspect from (joyent/node)
+  * added mocha for testing
+  * browser build for current api
+  * multiple .property assertions
+  * added deep equal from node
+
 0.0.2 / 2011-12-07
 ==================
 
--- chai.js
+++ chai.js
@@ -47,6 +47,16 @@ require.relative = function (parent) {
 
 
 require.register("assertion.js", function(module, exports, require){
+/*!
+ * chai
+ * Copyright(c) 2011 Jake Luer <jake@alogicalparadox.com>
+ * MIT Licensed
+ *
+ * Primarily a refactor of: should.js
+ * https://github.com/visionmedia/should.js
+ * Copyright(c) 2011 TJ Holowaychuk <tj@vision-media.ca>
+ * MIT Licensed
+ */
 
 var AssertionError = require('./error')
   , eql = require('./utils/eql')
@@ -73,6 +83,15 @@ Assertion.prototype.assert = function (expr, msg, negateMsg) {
 
 Assertion.prototype.__defineGetter__('inspect', function () {
   return inspect(this.obj);
+});
+
+Assertion.prototype.__defineGetter__('arguments', function () {
+  this.assert(
+      '[object Arguments]' == Object.prototype.toString.call(this.obj)
+    , 'expected ' + this.inspect + ' to be arguments'
+    , 'expected ' + this.inspect + ' to not be arguments');
+
+  return this;
 });
 
 Assertion.prototype.__defineGetter__('to', function () {
@@ -427,15 +446,17 @@ Assertion.prototype.throw = function (constructor) {
 }); // module: assertion.js
 
 require.register("chai.js", function(module, exports, require){
+/*!
+ * chai
+ * Copyright(c) 2011 Jake Luer <jake@alogicalparadox.com>
+ * MIT Licensed
+ */
 
 var exports = module.exports = {};
 
-exports.version = '0.0.2';
+exports.version = '0.1.0';
 
-exports.expect = function (val, message) {
-  return new exports.Assertion(val, message);
-};
-
+exports.expect = require('./interface/expect');
 exports.assert = require('./interface/assert');
 exports.should = require('./interface/should');
 
@@ -455,11 +476,20 @@ exports.fail = function (actual, expected, message, operator, stackStartFunction
 }); // module: chai.js
 
 require.register("error.js", function(module, exports, require){
+/*!
+ * chai
+ * Copyright(c) 2011 Jake Luer <jake@alogicalparadox.com>
+ * MIT Licensed
+ */
 
 var fail = require('./chai').fail;
 
 module.exports = AssertionError;
 
+/*!
+ * Inspired by node.js assert module
+ * https://github.com/joyent/node/blob/f8c335d0caf47f16d31413f89aa28eda3878e3aa/lib/assert.js
+ */
 function AssertionError (options) {
   options = options || {};
   this.name = 'AssertionError';
@@ -472,7 +502,7 @@ function AssertionError (options) {
   if (Error.captureStackTrace) {
     Error.captureStackTrace(this, stackStartFunction);
   }
-};
+}
 
 AssertionError.prototype.__proto__ = Error.prototype;
 
@@ -490,6 +520,12 @@ AssertionError.prototype.toString = function() {
 }); // module: error.js
 
 require.register("interface/assert.js", function(module, exports, require){
+/*!
+ * chai
+ * Copyright(c) 2011 Jake Luer <jake@alogicalparadox.com>
+ * MIT Licensed
+ */
+
 var Assertion = require('../assertion');
 
 var assert = module.exports = {};
@@ -506,20 +542,12 @@ assert.notEqual = function (act, exp, msg) {
   new Assertion(act, msg).to.not.equal(exp);
 };
 
-assert.strictEqual = function (act, exp, msg) {
-
-};
-
-assert.strictNotEqual = function (Act, exp, msg) {
-
-};
-
 assert.deepEqual = function (act, exp, msg) {
-
+  new Assertion(act, msg).to.eql(exp);
 };
 
 assert.notDeepEqual = function (act, exp, msg) {
-
+  new Assertion(act, msg).to.not.eql(exp);
 };
 
 assert.isTrue = function (val, msg) {
@@ -595,15 +623,28 @@ assert.throws = function (fn, type, msg) {
 };
 }); // module: interface/assert.js
 
-require.register("interface/should.js", function(module, exports, require){
-
+require.register("interface/expect.js", function(module, exports, require){
 /*!
- * Originally used by:
- * 
- * Should
- * Copyright(c) 2010 TJ Holowaychuk <tj@vision-media.ca>
+ * chai
+ * Copyright(c) 2011 Jake Luer <jake@alogicalparadox.com>
  * MIT Licensed
  */
+
+var Assertion = require('../assertion');
+
+module.exports = function (val, message) {
+  return new Assertion(val, message);
+};
+}); // module: interface/expect.js
+
+require.register("interface/should.js", function(module, exports, require){
+/*!
+ * chai
+ * Copyright(c) 2011 Jake Luer <jake@alogicalparadox.com>
+ * MIT Licensed
+ */
+
+var Assertion = require('../assertion');
 
 /**
  * Expose api via `Object#should`.
@@ -611,9 +652,13 @@ require.register("interface/should.js", function(module, exports, require){
  * @api public
  */
 
-var Assertion = require('../assertion');
-
 module.exports = function () {
+
+  /*!
+   * Originally from: should.js
+   * Copyright(c) 2010 TJ Holowaychuk <tj@vision-media.ca>
+   * MIT Licensed
+   */
   Object.defineProperty(Object.prototype, 'should', {
     set: function(){},
     get: function(){
@@ -621,20 +666,20 @@ module.exports = function () {
     },
     configurable: true
   });
-  
+
   var should = {};
-  
+
   should.equal = function (val1, val2) {
     new Assertion(val1).to.equal(val2);
   };
-  
+
   return should;
 };
 }); // module: interface/should.js
 
 require.register("utils/eql.js", function(module, exports, require){
-// Most of this is directly from
-// https://github.com/joyent/node/blob/master/lib/assert.js
+// This is directly from Node.js assert
+// https://github.com/joyent/node/blob/f8c335d0caf47f16d31413f89aa28eda3878e3aa/lib/assert.js
 
 
 module.exports = _deepEqual;
@@ -736,7 +781,8 @@ function objEquiv(a, b) {
 }); // module: utils/eql.js
 
 require.register("utils/inspect.js", function(module, exports, require){
-// Refactored rom nodejs/lib/utils.js
+// This is (almost) directly from Node.js utils
+// https://github.com/joyent/node/blob/f8c335d0caf47f16d31413f89aa28eda3878e3aa/lib/util.js
 
 module.exports = inspect;
 
