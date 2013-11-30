7a062a63d377579764642d3df92ed227783d94e1
Tim Ruffles <timruffles@googlemail.com>
2013-11-30 15:42:03 +0000

    add change assertion - enforce a change, optionally from and to specific values, optionally by a numeric diff

--- test/assert.js
+++ test/assert.js
@@ -633,4 +633,53 @@ describe('assert', function () {
     }, 'expected [ 1, 54 ] to have the same members as [ 6, 1, 54 ]');
   });
 
+
+  
+  it('change', function() {
+    var value = "start";
+
+    t(function() { assert.change(changeValue("end"),getValue); });
+    t(function() { assert.change(changeValue("end"),getValue,{from: "start"}); });
+    t(function() { assert.change(changeValue("end"),getValue,{to: "end"}); });
+
+    value = 10;
+    assert.change(changeValue(15),getValue,{by: 5});
+
+    t(function() { assert.noChange(noop,getValue) });
+    t(function() { assert.noChange(noop,getValue,{from: "start"}) });
+
+    t(function() { assert.noChange(noop,getValue,{to: "whevil"}) });
+   
+    // test examples
+    var x = 0;
+    assert.change(function() { x += 1; },function() { return x });
+ 
+    assert.noChange(function() {},function() { return x });
+ 
+    assert.change(function() { x += 1 },function() { return x },{by: 1});
+    assert.change(function() { x += 1 },function() { return x },{from: x});
+    assert.change(function() { x += 1 },function() { return x },{from: x, to: x + 1});
+    assert.change(function() { x += 1 },function() { return x },{to: x + 1});
+
+    var x = 0;
+    assert.noChange(doesNothing,function() { return x });
+    function doesNothing() {}
+   
+
+    function getValue() {
+      return value;
+    }
+    function changeValue(to) {
+      return function() {
+        value = to;
+      }
+    }
+    function t(testFn) {
+      testFn();
+      value = "start";
+    }
+    function noop() {}
+  });
+
+
 });
--- test/expect.js
+++ test/expect.js
@@ -773,4 +773,108 @@ describe('expect', function () {
     expect([5, 4]).not.members([5, 4, 2]);
   })
 
+  
+  describe('change assertion enforces changes',function() {
+
+    describe("of any kind",function() {
+      it("by throwing when change doesn't occur",function() {
+        err(function() {
+          var value = "start";
+          expect(function() {}).to.change(function() {
+            return value;
+          });
+        },"expected value to have changed from 'start'");
+      });
+      it("doesn't create false negatives",function() {
+        var value = "start";
+        expect(function() {
+          value = "end";
+        }).to.change(function() {
+          return value;
+        });
+      });
+    });
+
+    describe("from a specific value",function() {
+      it("by throwing when value wasn't correct before",function() {
+        err(function() {
+          expect(function() {}).to.change(function() {
+            return "start";
+          },{from: "fnord"});
+        },"change 'from' value wasn't equal to 'start'");
+      });
+      it("doesn't create false negatives",function() {
+        var value = "start";
+        expect(function() {
+          value = "end";
+        }).to.change(function() {
+          return value;
+        },{from: "start"});
+      });
+    });
+    describe("to a specific value",function() {
+      it("by throwing when value doesn't change to a specific value",function() {
+        err(function() {
+          var value = "start";
+          expect(function() {
+            value = "something else";
+          }).to.change(function() {
+            return value;
+          },{to: "end"});
+        },"expected 'start' to change to 'end', instead changed to 'something else'");
+      });
+      it("doesn't create false negatives",function() {
+        var value = "start";
+        expect(function() {
+          value = "end";
+        }).to.change(function() {
+          return value;
+        },{to: "end"});
+      });
+    });
+    describe("by a specific amount",function() {
+      it("by throwing",function() {
+        err(function() {
+          var value = 10;
+          expect(function() {
+            value = 15;
+          }).to.change(function() {
+            return value;
+          },{by: 10});
+        },"expected 10 to change to 20, instead changed to 15");
+      });
+      it("doesn't create false negatives",function() {
+        var value = 10;
+        expect(function() {
+          value = 20;
+        }).to.change(function() {
+          return value;
+        },{by: 10});
+
+      });
+      it("validates a numeric change",function() {
+        err(function() {
+          expect(noop).to.change(noop,{by: "fnord"});
+        },'change "by" assertions only work with numbers specified in "by" and or "from" options');
+        err(function() {
+          expect(noop).to.change(noop,{by: 5,from:"fnord"});
+        },'change "by" assertions only work with numbers specified in "by" and or "from" options');
+        function noop() {}
+      });
+
+    });
+    it("has working examples",function() {
+      var x = 0;
+      expect(function() { x += 1; }).to.change(function() { return x });
+     
+      expect(function() {}).not.to.change(function() { return x });
+     
+      expect(function() { x += 1 }).to.change(function() { return x },{by: 1});
+      expect(function() { x += 1 }).to.change(function() { return x },{from: x});
+      expect(function() { x += 1 }).to.change(function() { return x },{from: x, to: x + 1});
+      expect(function() { x += 1 }).to.change(function() { return x },{to: x + 1});
+    });
+
+  });
+
 });
