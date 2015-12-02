0fe59dace44e4b9011ec85a3759d3e628b3db402
Andrii Shumada <eagleeyes91@gmail.com>
2015-12-02 10:34:53 +0200

    Merge pull request #22 from debitoor/compare-dates
    Comparison of Dates

--- test/unit/chai-subset.spec.js
+++ test/unit/chai-subset.spec.js
@@ -140,3 +140,29 @@ describe('assert style of test', function () {
 		assert.containSubset({a: 1, b: 2}, {a: 1});
 	});
 });
+
+describe('comparison of dates', function() {
+	it('should pass for the same date', function() {
+		expect(new Date('2015-11-30')).to.containSubset(new Date('2015-11-30'));
+	});
+
+	it('should pass for the same date if nested', function() {
+		expect({a: new Date('2015-11-30')}).to.containSubset({a: new Date('2015-11-30')});
+	});
+
+	it('should fail for a different date', function() {
+		expect(new Date('2015-11-30')).to.not.containSubset(new Date('2012-02-22'));
+	});
+
+	it('should fail for a different date if nested', function() {
+		expect({a: new Date('2015-11-30')}).to.not.containSubset({a: new Date('2012-02-22')});
+	});
+
+	it('should fail for invalid expected date', function() {
+		expect(new Date('2015-11-30')).to.not.containSubset(new Date('not valid date'));
+	});
+
+	it('should fail for invalid actual date', function() {
+		expect(new Date('not valid actual date')).to.not.containSubset(new Date('not valid expected date'));
+	});
+});
