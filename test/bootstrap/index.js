ea2c7040aba6f5123c94893eb61bd0b54156c676
meeber <meeber@gametypething.com>
2016-04-11 22:31:41 -0400

    Refactor global.err to test error properties
    - Add feature https://github.com/chaijs/chai/issues/675
    - Fix bug https://github.com/chaijs/chai/issues/648
    - Allow object overload of global.err's second argument
    - Combine global.err and window.err into one file and function
    - Add tests for global.err

--- test/bootstrap/index.js
+++ test/bootstrap/index.js
@@ -1,22 +1,39 @@
-/*!
- * Attach chai to global
+if (typeof window === 'object') {
+  global = window;
+} else {
+  global.chai = require('../..');
+}
+
+/**
+ * Validate that the given function throws an error. Optionally validate some
+ * additional properties of the error:
+ *
+ * If val is a string, validate val equals the error's .message
+ * If val is a regex, validate val matches the error's .message
+ * If val is an object, validate val's props are included in the error object
+ *
+ * @param {Function} function that's expected to throw an error
+ * @param {Mixed} expected properties of the expected error
  */
 
-global.chai = require('../..');
+global.err = function (fn, val) {
+  if (chai.util.type(fn) !== 'function')
+    throw new chai.AssertionError('Invalid fn');
 
-/*!
- * Provide check for fail function.
- */
-
-global.err = function (fn, msg) {
   try {
     fn();
-    throw new chai.AssertionError({ message: 'Expected an error' });
   } catch (err) {
-    if ('string' === typeof msg) {
-      chai.expect(err.message).to.equal(msg);
-    } else {
-      chai.expect(err.message).to.match(msg);
+    switch (chai.util.type(val)) {
+      case 'undefined': return;
+      case 'string': return chai.expect(err.message).to.equal(val);
+      case 'regexp': return chai.expect(err.message).to.match(val);
+      case 'object': return Object.keys(val).forEach(function (key) {
+        chai.expect(err).to.have.property(key).and.to.deep.equal(val[key]);
+      });
     }
+
+    throw new chai.AssertionError('Invalid val');
   }
+
+  throw new chai.AssertionError('Expected an error');
 };
