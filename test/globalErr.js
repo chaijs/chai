8dc92d870a1e88a1f8fa70fabfb423c9f0709e1c
Ruwan Pradeep Geeganage <rpgeeganage@users.noreply.github.com>
2019-06-25 19:39:16 +0200

    Adding operator attribute to assertion error (#1257)
    * feat: adding operator attribute to assertion error
    
    You can set the operator as mentioned below.
    flag(this, 'operator', MYOPERATOR).
    for example,
    flag(obj, 'operator', 'notEqual')

--- test/globalErr.js
+++ test/globalErr.js
@@ -43,6 +43,115 @@ describe('globalErr', function () {
     });
   });
 
+  it('should pass operator if possible during none object comparison', function () {
+    err(function () {
+      expect('cat').to.equal('dog');
+    }, {
+        message: 'expected \'cat\' to equal \'dog\''
+      , expected: 'dog'
+      , actual: 'cat'
+      , operator: 'strictEqual'
+    });
+
+    err(function () {
+      expect('cat').to.not.equal('cat');
+    }, {
+      message: 'expected \'cat\' to not equal \'cat\''
+      , expected: 'cat'
+      , actual: 'cat'
+      , operator: 'notStrictEqual'
+    });
+  });
+
+  it('should pass operator if possible during plain object comparison', function () {
+    var val1 = {
+      propVal1: 'val1'
+    };
+
+    var val2 = {
+      propVal2: 'val2'
+    };
+
+    err(function () {
+      expect(val1).to.equal(val2);
+    }, {
+        message: "expected { propVal1: 'val1' } to equal { propVal2: 'val2' }"
+      , expected: val2
+      , actual: val1
+      , operator: 'deepStrictEqual'
+    });
+
+    err(function () {
+      expect(val1).to.not.equal(val1);
+    }, {
+      message: "expected { propVal1: 'val1' } to not equal { propVal1: 'val1' }"
+      , expected: val1
+      , actual: val1
+      , operator: 'notDeepStrictEqual'
+    });
+  });
+
+  it('should pass operator if possible during function comparison', function () {
+    function f1 () {
+      this.propF1 = 'propF1';
+    }
+
+    function f2 () {
+      this.propF2 = 'propF2';
+    }
+
+    err(function () {
+      expect(f1).to.equal(f2);
+    }, {
+        message: "expected [Function: f1] to equal [Function: f2]"
+      , expected: f2
+      , actual: f1
+      , operator: 'deepStrictEqual'
+    });
+
+    err(function () {
+      expect(f1).to.not.equal(f1);
+    }, {
+      message: "expected [Function: f1] to not equal [Function: f1]"
+      , expected: f1
+      , actual: f1
+      , operator: 'notDeepStrictEqual'
+    });
+  });
+
+  it('should pass operator if possible during object comparison', function () {
+    var val1 = [
+        'string1'
+      , 'string2'
+      , 'string3'
+      , 'string4'
+    ];
+
+    var val2 = [
+      'string5'
+    , 'string6'
+    , 'string7'
+    , 'string8'
+    ];
+    err(function () {
+      expect(val1).to.equal(val2);
+    }, {
+        message: 'expected [ Array(4) ] to equal [ Array(4) ]'
+      , expected: val2
+      , actual: val1
+      , operator: 'deepStrictEqual'
+    });
+
+    err(function () {
+      expect(val1).to.not.equal(val1);
+    }, {
+      message: 'expected [ Array(4) ] to not equal [ Array(4) ]'
+      , expected: val1
+      , actual: val1
+      , operator: 'notDeepStrictEqual'
+    });
+  });
+
   it('should throw if regex val does not match error message', function () {
     err(function () {
       err(function () { throw new Err('cat') }, /dog/);
