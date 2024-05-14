a77feedb39944d335180e3ee87d64f7135ba8286
Kristján Oddsson <koddsson@gmail.com>
2024-05-14 10:38:00 +0200

    enable chaining of chai plugins

--- test/plugins.js
+++ test/plugins.js
@@ -1,42 +1,40 @@
-import * as chai from '../index.js';
+import {use, expect} from '../index.js';
+
+function plugin(chai) {
+  if (chai.Assertion.prototype.testing) return;
+
+  Object.defineProperty(chai.Assertion.prototype, 'testing', {
+    get: function () {
+      return 'successful';
+    },
+  });
+}
+
+function anotherPlugin(chai) {
+  if (chai.Assertion.prototype.moreTesting) return;
+
+  Object.defineProperty(chai.Assertion.prototype, 'moreTesting', {
+    get: function () {
+      return 'more success';
+    },
+  });
+}
 
 describe('plugins', function () {
-
-  function plugin (chai) {
-    if (chai.Assertion.prototype.testing) return;
-
-    Object.defineProperty(chai.Assertion.prototype, 'testing', {
-      get: function () {
-        return 'successful';
-      }
-    });
-  }
-
   it('basic usage', function () {
-    chai.use(plugin);
-    var expect = chai.expect;
+    const {expect} = use(plugin);
     expect(expect('').testing).to.equal('successful');
   });
 
-  it('double plugin', function () {
-    chai.expect(function () {
-      chai.use(plugin);
+  it('multiple plugins', function () {
+    expect(function () {
+      use(plugin).use(anotherPlugin);
     }).to.not.throw();
   });
 
   it('.use detached from chai object', function () {
-    function anotherPlugin (chai) {
-      Object.defineProperty(chai.Assertion.prototype, 'moreTesting', {
-        get: function () {
-          return 'more success';
-        }
-      });
-    }
+    const {expect} = use(anotherPlugin);
 
-    var use = chai.use;
-    use(anotherPlugin);
-
-    var expect = chai.expect;
     expect(expect('').moreTesting).to.equal('more success');
   });
 });
